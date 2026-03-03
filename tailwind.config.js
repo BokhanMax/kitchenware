7c6a1a8d9da8c3c3b76a3d4d76c90f635c3a0665
Max Bokhan LA <bokhanworks@gmail.com>
2026-03-03 13:34:56 +0200

    re:

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    content: [
+        './src/**/*.html',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary-blue': '#3B82F6',
+                'light-blue': '#EFF6FF',
+                'dark-blue': '#1E40AF',
+				'bg-blue-primary': '#3B82F6'
+            }
+        }
+    },
+    plugins: [],
+};
